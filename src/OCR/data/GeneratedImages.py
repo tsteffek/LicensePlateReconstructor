822f667fc259bb9395dd435fe5fbeae9d50f43a9
tsteffek <thomas.steffek@beuth-hochschule.de>
2020-12-29 17:11:32 +0100

    feat(OCR/data): datasets and vocab
    We implement a vocabulary that can deal with noise characters, which will be mapped to a special index when encoding.
    
    We also add a PyTorch Dataset for our images, as well as the matching PyTorch Lightning DataModule.

---
+++ src/OCR/data/GeneratedImages.py
@@ -0,0 +1,113 @@
+import os
+from typing import Optional, List, Tuple, Iterable
+
+import numpy as np
+import pytorch_lightning as pl
+import torch
+from PIL import Image
+from torch import Tensor
+from torch.utils.data import Dataset, DataLoader
+
+from src.OCR import IO
+from src.OCR.data.model.Vocabulary import Vocabulary
+from src.OCR.image_gen.model.Text import Character, Text, TextImage
+
+
+class GeneratedImages(Dataset):
+    def __init__(self, path: str, vocab: Vocabulary, image_files: str = '**/*.jpg'):
+        super().__init__()
+        self.vocab = vocab
+        self.path = path
+        self.image_files = image_files
+
+        self.images = IO.get_image_paths(path, image_files)
+
+        w, h = IO.load_image(self.images[0]).size
+        self.size = h, w
+
+    def __getitem__(self, item) -> Tuple[Tensor, Tuple[Text, Tensor]]:
+        image_path = self.images[item]
+        txt_image = TextImage.load(image_path, self.vocab.languages)
+        txt, img, _ = txt_image
+        label = self.vocab.encode_text(txt_image.text)
+        return to_torch_format(txt_image.img), (txt_image.text, torch.tensor(label, dtype=torch.int64))
+
+    def __len__(self):
+        return len(self.images)
+
+
+def to_torch_format(img: Image) -> Tensor:
+    return torch.tensor(img, dtype=torch.float32).reshape(*img.size, -1).T / 255
+
+
+class GeneratedImagesDataModule(pl.LightningDataModule):
+    def __init__(self, path: str, batch_size, multi_core: bool = True, cuda: bool = torch.cuda.is_available(),
+                 shuffle: bool = True, language_file: str = 'languages.json', noise_file: str = 'noise.json', ):
+        super().__init__()
+        self.path = path
+        self.language_file = language_file
+        self.noise_file = noise_file
+        self.batch_size = batch_size
+        self.cuda = cuda
+        self.shuffle = shuffle
+        self.multi_core = multi_core
+        if multi_core:
+            self.num_workers = max(os.cpu_count(), batch_size)
+        else:
+            self.num_workers = 0
+
+        self.vocab = Vocabulary(IO.load_languages(path, language_file), noise=IO.read_json(noise_file))
+
+        self.train_dataset = None
+        self.test_dataset = None
+        self.val_dataset = None
+        self.size = None
+
+    def setup(self, stage: Optional[str] = None):
+        if stage == 'fit' or stage is None:
+            self.train_dataset = GeneratedImages(self.path, self.vocab, image_files='train/**/*.jpg')
+            self.val_dataset = GeneratedImages(self.path, self.vocab, image_files='val/**/*.jpg')
+            self.size = self.train_dataset.size
+        if stage == 'test' or stage is None:
+            self.test_dataset = GeneratedImages(self.path, self.vocab, image_files='test/**/*.jpg')
+            self.size = self.test_dataset.size
+
+    @staticmethod
+    def collate_fn(
+            batch: List[Tuple[Tensor, Tuple[Text, Tensor]]]
+    ) -> Tuple[Tensor, Tuple[Iterable[Text], Tensor, Tensor]]:
+        imgs, labels = list(zip(*batch))
+        texts, label_indices = list(zip(*labels))
+        label_lengths = torch.tensor([len(label) for label in label_indices], dtype=torch.int64)
+
+        return torch.stack(imgs), (np.array(texts, dtype=object), torch.cat(label_indices), label_lengths)
+
+    def train_dataloader(self) -> DataLoader:
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=self.cuda and self.multi_core,
+            shuffle=self.shuffle,
+            collate_fn=self.collate_fn
+        )
+
+    def test_dataloader(self) -> DataLoader:
+        return DataLoader(
+            self.test_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=self.cuda and self.multi_core,
+            shuffle=self.shuffle,
+            collate_fn=self.collate_fn
+        )
+
+    def val_dataloader(self) -> DataLoader:
+        return DataLoader(
+            self.val_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=self.cuda and self.multi_core,
+            shuffle=self.shuffle,
+            collate_fn=self.collate_fn
+        )
---
+++ src/OCR/data/model/Vocabulary.py
@@ -0,0 +1,51 @@
+from typing import Dict, Iterable, List
+
+from src.OCR.image_gen.model.Language import Language
+from src.OCR.image_gen.model.Text import Character, Text
+
+
+class Vocabulary:
+    def __init__(self, languages: Dict[int, Language], special_tokens: Iterable[str] = None,
+                 noise: Iterable[str] = None):
+        if special_tokens is None:
+            special_tokens = ['_', '°']
+        if noise is None:
+            noise = []
+
+        self.languages = languages
+        self.special_tokens = special_tokens
+
+        self.blank_idx = 0
+        self.noise_idx = 1
+
+        self.noise = set(noise)
+
+        self.chars = [*special_tokens]
+        self.noisy_chars = [*special_tokens]
+        self.offsets = [len(special_tokens)]
+        for idx in range(len(languages)):
+            lang = languages[idx]
+            assert idx == lang.id
+
+            chars = lang.chars
+            self.noisy_chars.extend(chars)
+
+            if self.noise is not None:
+                chars = [char for char in lang.chars if char not in self.noise]
+
+            self.chars.extend(chars)
+            self.offsets.append(self.offsets[idx] + len(chars))
+
+    def encode_char(self, char: Character):
+        if char.to_string() in self.noise:
+            return self.noise_idx
+        return self.offsets[char.language.id] + char.char_idx
+
+    def encode_text(self, text: Text):
+        return [self.encode_char(char) for char in text.chars]
+
+    def decode_char(self, char: int) -> str:
+        return self.chars[char]
+
+    def decode_text(self, text: Iterable[int]) -> List[str]:
+        return [self.decode_char(char) for char in text if char != self.blank_idx]
